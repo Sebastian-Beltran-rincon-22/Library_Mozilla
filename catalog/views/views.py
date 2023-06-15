5e32c82abb574dede103448df35d5460b2e72c71
johansebastianbeltran <sebastianrincon834@gmail.com>
2023-06-15 16:58:13 -0500

    update [views -> views.py]

--- catalog/views/views.py
+++ catalog/views/views.py
@@ -4,12 +4,16 @@ from django.shortcuts import render
 from ..models.author import Author
 from ..models.book import Book
 from ..models.book_instance import BookInstance
+from ..models.language import Language
+from ..models.genre import Genre
 
 def index(request):
     
     book = Book.objects.all()
     author = Author.objects.all()
     book_instance = BookInstance.objects.all()
+    language = Language.objects.all()
+    genre = Genre.objects.all()
     
     return render(
         request,
@@ -18,6 +22,8 @@ def index(request):
         {
             "book":book,
             "author":author,
-            "book_instance":book_instance
+            "book_instance":book_instance,
+            "language":language,
+            "genre":genre
         }
     )  
